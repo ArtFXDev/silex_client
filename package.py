deb69f4a5369bcb98a0b4c24be751d164b7c4006
Simon Lambin <simon.lamb98@gmail.com>
2021-06-01 17:13:47 +0200

    Create the rez package

---
+++ package.py
@@ -0,0 +1,39 @@
+name = "silex_client"
+
+version = "0.0.0"
+
+authors = ["ArtFx TD gang"]
+
+description = \
+    """
+    Set of modules used to execute actions on client's side
+    Part of the Silex ecosystem
+    """
+
+requires = ["python-3.7+", "PyYAML", "logzero"]
+
+vcs = "git"
+
+tests = {"unit": "python -m pytest {root}"}
+
+build_command = "python {root}/script/build.py {install}"
+
+build_requires = ["pytest"]
+
+
+def commands():
+    """
+    Set the environment variables for silex_client
+    """
+    env.PATH.append("{root}/silex_client")
+    env.PYTHONPATH.append("{root}/silex_client")
+    env.SILEX_LOG_LEVEL = "DEBUG"
+
+
+def uuid():
+    """
+    Automatically generate a uuid for the package
+    """
+    import uuid
+
+    return uuid.uuid4().hex
---
+++ script/build.py
@@ -0,0 +1,38 @@
+import shutil
+import sys
+import os
+
+
+def build(source_path, build_path, install_path, targets):
+    src = os.path.join(source_path, "silex_client")
+    config = os.path.join(source_path, "package.py")
+
+    if os.path.exists(build_path):
+        # Clear the build folder
+        try:
+            shutil.rmtree(os.path.join(build_path, "silex_client"))
+            os.remove(os.path.join(build_path, "package.py"))
+        except Exception as ex:
+            print("WARNING : Could not clear the build folder")
+            print(ex)
+
+    shutil.copytree(src, os.path.join(build_path, "silex_client"))
+    shutil.copy(config, build_path)
+
+    if "install" in (targets or []):
+        if os.path.exists(install_path):
+            try:
+                shutil.rmtree(install_path)
+            except Exception as ex:
+                print("WARNING : Could not clear the install folder")
+                print(ex)
+
+        shutil.copytree(src, install_path)
+        shutil.copy(config, install_path)
+
+
+if __name__ == '__main__':
+    build(source_path=os.environ['REZ_BUILD_SOURCE_PATH'],
+          build_path=os.environ['REZ_BUILD_PATH'],
+          install_path=os.environ['REZ_BUILD_INSTALL_PATH'],
+          targets=sys.argv[1:])
