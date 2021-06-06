bf92e235e928612503fd2c431ba0ee2b4dcbfdc2
Simon Lambin <slambin@artfx.fr>
2021-06-06 18:37:53 +0200

    Create the stop() method

--- silex_client/utils/websocket.py
+++ silex_client/utils/websocket.py
@@ -21,6 +21,7 @@ class WebsocketClient():
     """
     def __init__(self, url="ws://localhost:8080"):
         self.is_running = False
+        self.thread = None
         self.url = url
         self.loop = asyncio.new_event_loop()
 
@@ -55,6 +56,7 @@ class WebsocketClient():
         """
         # TODO: Define a json protocol and handle the messages accordingly
         logger.info("Websocket message recieved : %s", message)
+        print('Active tasks count: ', len([task for task in asyncio.Task.all_tasks() if not task.done()]))
 
     def _start_loop(self, loop):
         """
@@ -63,15 +65,20 @@ class WebsocketClient():
         """
         asyncio.set_event_loop(loop)
         self.loop.create_task(self._receive_message())
-        self.loop.run_forever()
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            logger.info("Caught keyboard interrupt, stopping server...")
+            self.stop()
 
     def run(self):
         """
         Initialize the event loop's task and run it in main thread
         """
-        if self.is_running == True:
+        if self.is_running:
             logger.warn("Websocket server already running")
             return
+
         self.is_running = True
         self._start_loop(self.loop)
 
@@ -80,8 +87,28 @@ class WebsocketClient():
         """
         Initialize the event loop's task and run it in a different thread
         """
-        if self.is_running == True:
+        if self.is_running:
             logger.warn("Websocket server already running")
             return
+
+        self.is_running = True
+        self.thread = Thread(target=lambda: self._start_loop(self.loop))
+        self.thread.start()
+
+    def stop(self):
+        """
+        Stop the event loop event loop wherever it was run from
+        (main thread or other thread)
+        """
+        if not self.is_running:
+            logger.info("Websocket server was not running")
+            return
+
+        # Clear the loop
+        self.loop.stop()
+        # TODO: Find a way to clear the loop instead of let it get garbage collected
+        self.loop = asyncio.new_event_loop()
+        # If the loop was running in a different thread stop it
+        if self.thread is not None:
+            self.thread.join()
         self.is_running = False
-        Thread(target=lambda: self._start_loop(self.loop)).start()
