c1767c199808fd8c49673c5c5ea4c2245a1bc768
Simon Lambin <simon.lamb98@gmail.com>
2021-06-08 10:32:25 +0200

    Handle disconnection

--- silex_client/utils/websocket.py
+++ silex_client/utils/websocket.py
@@ -21,10 +21,8 @@ class WebsocketConnection:
     Websocket client that connect the the given url
     and receive and handle the incomming messages
     """
-
     def __init__(self, url="ws://localhost:8080"):
         self.is_running = False
-        self.pending_stop = False
         self.thread = None
         self.url = url
         self.loop = asyncio.new_event_loop()
@@ -32,39 +30,45 @@ class WebsocketConnection:
     async def _connect(self):
         """
         Connect to the server, wait for the incomming messages and handle disconnection
+        Called by self.run() or self.run_multithreaded()
         """
+        self.pending_restart = False
         try:
             async with websockets.connect(self.url) as websocket:
+                # Initialize the connection by sending the context's metadata
                 await websocket.send(str(context.metadata))
-                self.loop.create_task(self._receive_message(websocket))
-                self.loop.create_task(self._listen_event(websocket))
-                while True:
-                    if self.pending_stop:
-                        asyncio.get_event_loop().stop()
-                    else:
-                        await asyncio.sleep(1)
-        except OSError:
+                # Create two async tasks
+                self.loop.create_task(self._listen_incomming(websocket))
+                self.loop.create_task(self._listen_outgoing(websocket))
+                # Listen to pending stop or restart
+                while not self.pending_stop and not self.pending_restart:
+                    await asyncio.sleep(1)
+        except (OSError, ConnectionResetError,
+                websockets.exceptions.InvalidMessage):
             logger.warning("Could not connect to %s retrying...", self.url)
-            # Restart the loop to retry connection
-            await asyncio.sleep(1)
-            self.loop.create_task(self._connect())
+            self.pending_restart = True
+        logger.info("Leaving event loop...")
 
-    async def _receive_message(self, websocket):
-        while True:
+    async def _listen_incomming(self, websocket):
+        """
+        Async infinite loop waiting for messages to receive
+        """
+        while not self.pending_stop and not self.pending_restart:
             try:
-                message = await websocket.recv()
                 # The queue of incomming message is already handled by the library
+                message = await websocket.recv()
                 await self._handle_message(message, websocket)
-            except websockets.ConnectionClosed:
-                logger.warning("Websocket connection on %s lost retrying...", self.url)
-                # TODO: Handle the restart of the connection
+            except (websockets.ConnectionClosed,
+                    websockets.exceptions.ConnectionClosedError):
+                logger.warning("Connection on %s lost retrying...", self.url)
+                self.pending_restart = True
                 break
 
-    async def _listen_event(self, websocket):
+    async def _listen_outgoing(self, websocket):
         """
-        Parse the incomming messages and run appropriate function
+        Async infinite loop waiting for messages to be sent
         """
-        while True:
+        while not self.pending_stop and not self.pending_restart:
             # TODO: Execute all the pending event in the list
             await asyncio.sleep(1)
 
@@ -88,16 +92,25 @@ class WebsocketConnection:
             return
 
         asyncio.set_event_loop(self.loop)
-        self.loop.create_task(self._connect())
-        try:
-            self.loop.run_forever()
-        except KeyboardInterrupt:
-            # Catch keyboard interrupt to allow stopping the event loop with ctrl+c
-            self.stop()
-        print("loop completed")
-        # Clean the event loop once completed
-        self._clear_loop()
-        self.pending_stop = False
+
+        while True:
+            self.pending_stop = False
+            self.pending_restart = False
+
+            try:
+                self.loop.run_until_complete(self._connect())
+            except KeyboardInterrupt:
+                # Catch keyboard interrupt to allow stopping the event loop with ctrl+c
+                self.stop()
+
+            # Clean the event loop once completed
+            self._clear_loop()
+
+            # If no restart is required leave the loop
+            if not self.pending_restart:
+                break
+
+        self.is_running = False
 
     def _clear_loop(self):
         """
@@ -105,10 +118,10 @@ class WebsocketConnection:
         This method is called by self._start_loop()
         """
         if self.loop.is_running():
-            logger.info("The event loop needs to be stopped before being cleared")
+            logger.info("The event loop must be stopped before being cleared")
             return
 
-        # TODO: Find a way to clear the loop that works properly everytime
+        # TODO: Find a way to clear the loop that works without warnings everytime
         logger.info("Clearing event loop...")
         # Clear the loop
         for task in asyncio.Task.all_tasks():
@@ -118,12 +131,12 @@ class WebsocketConnection:
         with suppress(asyncio.CancelledError):
             for task in asyncio.Task.all_tasks():
                 self.loop.run_until_complete(task)
+
         # Create a new loop and let the old one get garbage collected
         self.loop = asyncio.new_event_loop()
         # Trigger the garbage collection
         gc.collect()
 
-        self.is_running = False
         logger.info("Event loop cleared")
 
     def run(self):
@@ -159,8 +172,7 @@ class WebsocketConnection:
         if self.thread is not None:
             self.thread.join(3)
             if self.thread.is_alive():
-                logger.warn(
-                    "Could not stop the websocket connection thread for %s", self.url
-                )
+                logger.warn("Could not stop the connection thread for %s",
+                            self.url)
             else:
                 self.thread = None
