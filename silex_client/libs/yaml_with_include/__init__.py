4aa0080bf1eb307b0c77fd892ec27f1f248c96ac
OlivierArgentieri <olivierargentieri@gmail.com>
2021-06-10 15:51:01 +0200

    refactor file to respect pylint

--- silex_client/libs/yaml_with_include/__init__.py
+++ silex_client/libs/yaml_with_include/__init__.py
@@ -1,11 +1,16 @@
-import yaml
+"""
+  YamlWithInclude entry file
+"""
+
 import os
 import logging
+import yaml
 
 class YamlWithInclude(yaml.SafeLoader):
     """
-
+      Class YamlWithInclude
     """
+
     @staticmethod
     def load(file_to_open):
         """
@@ -13,19 +18,19 @@ class YamlWithInclude(yaml.SafeLoader):
         """
         buffer = "" # to return complete file
         try:
-            with open(file_to_open) as f:
-                for l in f.readlines():
-                    if "#" in l:
-                        l = l.split("#")[0] # exclude all comment things
-                    if "!include" in l:
-                        path_to_include = l.rstrip().replace("!include ", '')
+            with open(file_to_open) as file:
+                for line in file.readlines():
+                    if "#" in line:
+                        line = line.split("#")[0] # exclude all comment things
+                    if "!include" in line:
+                        path_to_include = line.rstrip().replace("!include ", '')
                         try:
-                            
-                            # clean and get current path 
-                            current_path = file_to_open.replace('/', os.sep).replace('\\', os.sep).replace(f"\\", os.sep)
+
+                            # clean and get current path
+                            current_path = file_to_open.replace('/', os.sep).replace('\\', os.sep).replace("\\", os.sep)
                             current_path = current_path.replace(current_path.split(os.path.sep)[-1], '')
                             # clean include path
-                            clean_include_path = path_to_include.replace('/', os.sep).replace('\\', os.sep).replace(f"\\", os.sep)
+                            clean_include_path = path_to_include.replace('/', os.sep).replace('\\', os.sep).replace("\\", os.sep)
 
                             # open include file
                             include_path = os.path.join(current_path, clean_include_path)
@@ -33,10 +38,10 @@ class YamlWithInclude(yaml.SafeLoader):
                                 #read content of the include file and add it to the main buffer
                                 buffer += include_file.read()
                                 buffer += "\r"
-                        except Exception as e:
-                            logging.error(f"Error on include parsing :{e}")
+                        except Exception as err:
+                            logging.error(f"Error on include parsing :{err}")
                     else:
-                        buffer += l # add line to complete file
-        except Exception as e:
-            logging.error(f"error on load .yaml file: {e}")
+                        buffer += line # add line to complete file
+        except Exception as err:
+            logging.error(f"error on load .yaml file: {err}")
         return yaml.load(buffer, YamlWithInclude)
