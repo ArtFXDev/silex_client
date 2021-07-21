b7673c9be489806aa313f7fd6c17e9e1ddae1d68
unknown <Simon Lambin>
2021-07-21 22:03:54 +0200

    Inplement the ActionQuery and ActionBuffer's method abstractly

--- silex_client/action/buffer.py
+++ silex_client/action/buffer.py
@@ -1,6 +1,67 @@
+import uuid
+
+from silex_client.network.websocket import WebsocketConnection
+
+
 class ActionBuffer():
     """
     Store the state of an action, it is used as a comunication entry with the UI
     """
-    def __init__(self):
+    def __init__(self, ws_connection: WebsocketConnection):
+        self.ws_connection = ws_connection
+        self.uid = uuid.uuid1()
+
+        self._parameters = {}
+        self._variables = {}
+        self._commands = {}
+
+    def _serialize(self):
+        """
+        Convert the action's data into json so it can be sent to the UI
+        """
         pass
+
+    def _deserialize(self, serealised_data):
+        """
+        Convert back the action's data from json into this object
+        """
+        pass
+
+    def send(self):
+        """
+        Serialize and send this buffer to the UI though websockets
+        """
+        pass
+
+    def receive(self, timeout: int):
+        """
+        Wait for the UI to send back a buffer and deserialize it
+        """
+        pass
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, parameters: dict):
+        # TODO: Check if the given parameters are correct
+        self._parameters.update(parameters)
+
+    @property
+    def variables(self):
+        return self._variables
+
+    @variables.setter
+    def variables(self, variables: dict):
+        # TODO: Check if the given variables are correct
+        self._variables.update(variables)
+
+    @property
+    def commands(self):
+        return self._commands
+
+    @commands.setter
+    def commands(self, commands: dict):
+        # TODO: Check if the given commands are correct
+        self._commands.update(commands)
--- silex_client/action/query.py
+++ silex_client/action/query.py
@@ -1,6 +1,61 @@
+import importlib
+from typing import Union
+
+from silex_client.network.websocket import WebsocketConnection
+from silex_client.action.config import ActionConfig
+from silex_client.action.buffer import ActionBuffer
+from silex_client.utils.log import logger
+
+
 class ActionQuery():
     """
     Initialize and execute a given action
     """
-    def __init__(self):
+    def __init__(self, action_name: str, ws_connection: WebsocketConnection):
+        self.action_name = action_name
+        self.config = ActionConfig()
+        self.buffer = ActionBuffer(ws_connection)
         pass
+
+    def execute(self) -> ActionBuffer:
+        """
+        Execute the action's commands in order,
+        send and receive the buffer to the UI when nessesary
+        """
+        return self.buffer
+
+    def _initialize_buffer(self) -> None:
+        """
+        Initialize the buffer from the config
+        """
+        pass
+
+    def _run_command(self, command: str, args: Union[list, dict,
+                                                     tuple]) -> None:
+        """
+        Import the modules nessesary to the given command and run it
+        """
+        # Import the command
+        split_module = command.split(".")
+        try:
+            command_module = importlib.import_module(
+                command.replace("." + split_module[-1], ""))
+        except ImportError:
+            logger.error("Command %s not found", command)
+            return
+
+        command_attribute = getattr(command_module, split_module[-1])
+        # Test if the command is callable
+        if not callable(command):
+            logger.error("Command %s is not callable", command_attribute)
+            return
+
+        # Execute the command with arg parameters
+        if type(args) in (list, tuple):
+            command_attribute(*args)
+        # Execute the command with kwargs parameters
+        elif type(args) is dict:
+            command_attribute(**args)
+        else:
+            logger.error("Invalid arugment for command %s", command)
+            return
