91152cff180fe14eeb4d5032d25593781bb93839
Simon <slambin@artfx.fr>
2021-08-27 16:36:55 +0200

    Use subparser in cli argument

--- silex_client/cli/parser.py
+++ silex_client/cli/parser.py
@@ -5,11 +5,20 @@ def main():
     Parse the given arguments and call the appropriate handlers
     """
     parser = argparse.ArgumentParser(prog="silex", description="CLI for the silex pipeline ecosystem")
-    parser.add_argument('--context', type=str, help="Resolve and open an environment in the given context")
-    parser.add_argument('--dcc', type=str, help="Start the given DCC in the selected context or in the current one")
-    parser.add_argument('--action', type=str, help="Execute the given action in the selected context or in the current one")
-    parser.add_argument('--command', type=str, help="Execute the given command in the selected context or in the current one")
+    parser.add_argument('--context', type=str, help="Resolve and open an environment in the given context", dest="context")
+    parser.add_argument("--list", "-l", default=False, help="List the available options for the subcommand in the context", action="store_true")
+    subparsers = parser.add_subparsers(help="The action to perform under the given context", dest="subcommand")
+
+    action_parser = subparsers.add_parser("action", help="Execute the given action in the context")
+    command_parser = subparsers.add_parser("command", help="Execute the given command in the context")
+
+    action_parser.add_argument("action_name", help="The name of the action to perform under the context")
+
+    command_parser.add_argument("command_name", help="The python path of the command to perform under the context")
+
     args = parser.parse_args()
+
+    print(args)
 
 if __name__ == "__main__":
     main()
