c0e664386a77ce325bd2b3ab1ca318040d4a09d4
Simon <slambin@artfx.fr>
2021-08-28 10:21:57 +0200

    Implement the action cli command

--- silex_client/cli/handlers.py
+++ silex_client/cli/handlers.py
@@ -1,7 +1,8 @@
 from silex_client.utils.context import Context
 
+import pprint
 
-def action_handler(action_name: str, list_actions: bool=False):
+def action_handler(action_name: str, list_actions: bool=False) -> None:
     """
     Execute the given action in the current context
     """
@@ -9,4 +10,9 @@ def action_handler(action_name: str, list_actions: bool=False):
     context = Context.get()
     if list_actions:
         # Just print the available actions
+        print("Available actions :")
+        action_names = [action["name"] for action in context.config.actions]
+        pprint.pprint(action_names)
         return
+
+    context.get_action(action_name).execute()
