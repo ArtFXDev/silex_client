640c6232ad7bb4033fd39d8583dc6c65d85e5346
Simon <slambin@artfx.fr>
2021-09-13 09:08:22 +0200

    update the ReadOnlyDict class

--- silex_client/utils/datatypes.py
+++ silex_client/utils/datatypes.py
@@ -11,18 +11,6 @@ class ReadOnlyDict(dict):
     """
     Pointer to an editable dict. It allows to read its data but not to edit it
     """
-    def __init__(self, target: dict):
-        self._target = target
-
-    def __repr__(self) -> str:
-        return self._target.__repr__()
-
-    def __str__(self) -> str:
-        return self._target.__str__()
-
-    def __getitem__(self, key) -> Any:
-        return self._target[key]
-
     @staticmethod
     def __readonly__(*args, **kwargs) -> None:
         raise ReadOnlyError("This dictionary is readonly")
@@ -32,18 +20,3 @@ class ReadOnlyDict(dict):
     pop = __readonly__
     clear = __readonly__
     update = __readonly__
-
-    def get(self, key: str, default: Any = None):
-        try:
-            return self[key]
-        except KeyError:
-            return default
-
-    def items(self):
-        return self._target.items()
-
-    def keys(self):
-        return self._target.keys()
-
-    def values(self):
-        return self._target.values()
