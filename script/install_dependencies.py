d79a93078166944f10a275b75edd239ff99fd77d
Simon Lambin <slambin@artfx.fr>
2021-10-03 16:57:37 +0200

    Provide an install_dependencies helper script

---
+++ script/install_dependencies.py
@@ -0,0 +1,61 @@
+import importlib.util
+import os
+import subprocess
+import shutil
+
+from rez import resolved_context, package_maker
+from rez.utils import sourcecode
+
+
+def fix_rez_pip():
+    package_overrides = {
+        "commands":
+        sourcecode.SourceCode("env.PATH.append('{}')".format(
+            str(os.path.dirname(shutil.which("python")).replace(os.sep, "/"))))
+    }
+
+    current_context = resolved_context.ResolvedContext.get_current()
+    python_package = next(package
+                          for package in current_context.resolved_packages
+                          if package.name == "python").parent
+
+    with package_maker.make_package(name=python_package.name,
+                                    path=python_package.resource.location,
+                                    skip_existing=False) as new_package:
+        for key, value in python_package.validated_data().items():
+            setattr(new_package, key, value)
+
+        for key, value in package_overrides.items():
+            setattr(new_package, key, value)
+
+
+def get_requirements():
+    spec = importlib.util.spec_from_file_location(
+        "package", os.path.join(__file__, "..", "..", "package.py"))
+    package = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(package)
+    requirements = package.requires
+
+    for _, value in package.tests.items():
+        requirements += value.get("requires", [])
+
+    requirements.remove("python-3.7")
+    return requirements
+
+
+def main():
+    for requirement in get_requirements():
+        try:
+            return_value = subprocess.call(["rez", "pip", "-i", requirement])
+        except FileNotFoundError:
+            print("REZ could not be found on you system")
+            return
+
+        if return_value == 1:
+            fix_rez_pip()
+            main()
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ script/install_rez.ps1
@@ -0,0 +1,78 @@
+param([parameter(mandatory=$false)] [string]$rez_dir = "")
+
+function download-githubrelease {
+    param(
+        [parameter(mandatory=$true)] [string]$repo,
+        [parameter(mandatory=$false)] [string]$file = ""
+    )
+
+    $releases = "https://api.github.com/repos/$repo/releases"
+    # determining the latest release
+    $tag = (invoke-webrequest $releases -usebasicparsing | convertfrom-json)[0].tag_name
+
+    if ($file -eq "") {
+        $file = "$tag.zip"
+        $download = "https://github.com/$repo/archive/refs/tags/$file"
+        $name = "$($repo.split("/")[1])-$tag"
+    }
+    else {
+        $download = "https://github.com/$repo/releases/download/$tag/$file"
+        $name = $file.split(".")[0]
+    }
+
+    if ($file.split(".")[-1] -ne "zip") {
+        invoke-webrequest $download -out $file -usebasicparsing
+        return $file
+    }
+
+    $zip = "$name-$tag.zip"
+    $dir = "$name-$tag"
+
+    invoke-webrequest $download -out $zip -usebasicparsing
+    expand-archive $zip -force
+
+    # moving from temp dir to target dir
+    move-item $dir\$name -destination $name -force
+
+    # removing temp files
+    remove-item $zip -force
+    remove-item $dir -recurse -force
+
+    return $name
+}
+
+function install-rez {
+    if($null -ne $(get-command "rez" -erroraction silentlycontinue)) {
+        return
+    }
+    write-output "downloading rez..."
+    # set a default rez dir if not provided
+    if ($rez_dir -eq "") {
+        $rez_dir = "$home\rez"
+    }
+    $rez_source = download-githubrelease -repo "nerdvegas/rez"
+
+    # install rez
+    write-output "installing rez..."
+    invoke-expression "python $(resolve-path $rez_source)\install.py `"$rez_dir`""
+    $env:path=$env:path + ";$rez_dir\scripts\rez"
+    [environment]::setenvironmentvariable('path', $env:path, 'user')
+    remove-item $rez_source -recurse -force
+    $env:rez=$rez_dir
+    [environment]::setenvironmentvariable('rez', $env:rez, 'user')
+    $env:pythonpath=$env:pythonpath + ";$rez_dir\lib\site-packages"
+    [environment]::setenvironmentvariable('pythonpath', $env:pythonpath, 'user')
+
+}
+
+function setup-rez {
+    # create some default packages
+    write-output "binding default rez packages..."
+    rez-bind --quickstart
+    $install_dependencies = Join-Path -path $((Get-Item $PSCommandPath).Directory.FullName) -childPath "install_dependencies.py"
+    rez-env rez -- "python $install_dependencies"
+}
+
+# start the installation
+install-rez
+setup-rez
