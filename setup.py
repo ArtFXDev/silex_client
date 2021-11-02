c011e1f49ab2f86a24c7d538f2e4b55ecbfcf6b2
Simon Lambin <slambin@artfx.fr>
2021-11-02 16:20:44 +0100

    Use entry points

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 from distutils.util import convert_path
 
-# Get version without sourcing gazu module
+# Get version without sourcing silex module
 # (to avoid importing dependencies yet to be installed)
 main_ns = {}
 with open(convert_path("silex_client/__version__.py")) as ver_file:
@@ -11,4 +11,12 @@ with open(convert_path("silex_client/__version__.py")) as ver_file:
 setup(
     version=main_ns["__version__"],
     python_requires="==3.7.*",
+    entry_points={
+        "silex_action_config": [
+            "base=silex_client.config.entry_point:action_entry_points",
+        ],
+        "console_scripts": [
+            "silex=silex_client.cli.parser:main",
+        ],
+    },
 )
