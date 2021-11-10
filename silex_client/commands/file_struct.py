4c2d5eeaf7b8f9e30cc2979d961330f3f0bfe288
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-11-10 09:19:21 +0100

    refactor: add loggers

--- silex_client/commands/file_struct.py
+++ silex_client/commands/file_struct.py
@@ -19,55 +19,47 @@ class FileStructure(CommandBase):
     Copy file and override if necessary
     """
 
-    
-
     @CommandBase.conform_command()
     async def __call__(
         self, upstream: Any, parameters: Dict[str, Any], action_query: ActionQuery
     ):
 
         # get all project for the user
-        projects: Any = await gazu.project.all_open_projects()
+        projects: List[Dict[Any]] = await gazu.project.all_open_projects()
 
         if not projects:
             logger.error('No project found')
             return
-        
 
         # create structure for each project
         for project in projects:
-            
+
             # get tasks paths
             tasks: Any = await gazu.task.all_tasks_for_project(project)
 
-            if not tasks:
-                logger.error('no tasks found')
-                return
+            if tasks:
 
-            for task in tasks:
+                for task in tasks:
 
-                working_path = await gazu.files.build_working_file_path(
-                    task = task
-                )
+                    working_path = await gazu.files.build_working_file_path(
+                        task=task
+                    )
 
-                working_path_work: str = os.path.dirname(working_path)
-                working_path_work: str = working_path_work.replace('/', f'{os.path.sep}')
-                decompo: list[str] = working_path_work.split(os.path.sep)
+                    working_path_work: str = os.path.dirname(working_path)
+                    working_path_work: str = working_path_work.replace(
+                        '/', f'{os.path.sep}')
+                    decompo: list[str] = working_path_work.split(os.path.sep)
 
-                os.makedirs(working_path_work,exist_ok=True)
+                    os.makedirs(working_path_work, exist_ok=True)
 
-                # create sequences folder
-                if decompo[3] == "shots":
-                    try:
+                    # create sequences folder
+                    if decompo[3] == "shots":
                         sequ_path: str = f'{os.path.join(*decompo[:3])}{os.path.sep}sequences{os.path.sep}{decompo[4]}{os.path.sep}{decompo[6]}{os.path.sep}work'
-                        os.makedirs(sequ_path,exist_ok=True)
-                        logger.info('sequence folder created')
-                    except:
-                        logger.info('Could not create the sequence folder')
 
-                # create rushes folder
-                rushes: str = f'{os.path.join(*decompo[:3])}{os.path.sep}editing'
-                os.makedirs(rushes,exist_ok=True)
-                logger.info('rushes folder created')
+                    # create rushes folder
+                    rushes: str = f'{os.path.join(*decompo[:3])}{os.path.sep}editing'
+                logger.info('{} was created !'.format(project['name']))
 
-            logger.info(f'{project} was created !')
+            else:
+                logger.error('no tasks found')
+                logger.error('{} was not created !'.format(project['name']))
