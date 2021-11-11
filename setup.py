0ac312cd289fb73290733d3b26268bd3c4fd936f
Simon Lambin <slambin@artfx.fr>
2021-11-11 14:15:31 +0100

    Fix the pip package

--- setup.py
+++ setup.py
@@ -11,14 +11,4 @@ with open(convert_path("silex_client/__version__.py")) as ver_file:
 setup(
     version=main_ns["__version__"],
     python_requires="==3.7.*",
-    entry_points={
-        "silex_action_config": [
-            "base=silex_client.config.entry_point:action_entry_points",
-        ],
-        "console_scripts": [
-            "silex=silex_client.cli.parser:main",
-        ],
-    },
-    package_data={'': ["*.yaml", "*.yml", ".env"]},
-    include_package_data=True,
 )
