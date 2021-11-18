396b533c6916cb6a9758991739923cb4a84134d1
Simon Lambin <slambin@artfx.fr>
2021-11-18 15:07:09 +0100

    Use the refresh token route instead of the token route

--- silex_client/utils/authentification.py
+++ silex_client/utils/authentification.py
@@ -15,16 +15,20 @@ def authentificate_gazu() -> bool:
     async def get_authentification_token():
         async with aiohttp.ClientSession() as session:
             silex_service_host = os.getenv("SILEX_SERVICE_HOST", "")
-            async with session.get(f"{silex_service_host}/auth/token") as response:
+            async with session.get(f"{silex_service_host}/auth/refresh-token") as response:
                 return await response.json()
 
+    # Get the authentification token
     try:
         authentification_token = asyncio.run(get_authentification_token())
     except (ClientConnectionError, ContentTypeError):
         logger.error("Connection with the silex service could not be established")
         return False
 
+    # Set the authentification token
     gazu.client.set_tokens(authentification_token)
+
+    # Make sure the authentification worked
     try:
         asyncio.run(gazu.client.host_is_valid())
     except (ClientConnectionError):
