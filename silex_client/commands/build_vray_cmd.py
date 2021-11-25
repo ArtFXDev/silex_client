fb47b4fa92813525a43bb2b3dc379afe0378736e
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-11-25 17:57:13 +0100

    refactor: type fix

--- silex_client/commands/build_vray_cmd.py
+++ silex_client/commands/build_vray_cmd.py
@@ -120,7 +120,7 @@ class VrayCommand(CommandBase):
             raise Exception('No frame range found')
 
         # Create frame_lists with pading
-        frame_chunks: List[Any] = list(self._list_from_padding(
+        frame_chunks: List[str] = list(self._list_from_padding(
             list(range(frame_range[0], frame_range[1] + 1)),  frame_range[2]))
 
 
