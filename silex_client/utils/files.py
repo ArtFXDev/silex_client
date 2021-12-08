acba12960968315ea9886f3b71ddd512cfa1a782
Simon Lambin <slambin@artfx.fr>
2021-12-08 15:01:48 +0100

    Create the is_valid_path utility

--- silex_client/utils/files.py
+++ silex_client/utils/files.py
@@ -9,7 +9,13 @@ import importlib
 import os
 import pathlib
 import re
+import sys
+import errno
+
 from types import ModuleType
+
+# Sadly, Python fails to provide the following magic number for us.
+ERROR_INVALID_NAME = 123
 
 
 def reload_recursive(parent_module: ModuleType) -> None:
@@ -43,3 +49,39 @@ def is_valid_pipeline_path(file_path: pathlib.Path, path_type: str = "publish") 
     if re.search(r"^P:\\.+\\" + path_type + r"\\", str(file_path)) is not None:
         return True
     return False
+
+def is_valid_path(pathname: str) -> bool:
+    '''
+    `True` if the passed pathname is a valid pathname for the current OS;
+    `False` otherwise.
+    '''
+    # If this pathname is either not a string or is but is empty, this pathname
+    # is invalid.
+    try:
+        if not isinstance(pathname, str) or not pathname:
+            return False
+
+        _, pathname = os.path.splitdrive(pathname)
+
+        root_dirname = os.environ.get('HOMEDRIVE', 'C:') \
+            if sys.platform == 'win32' else os.path.sep
+        assert os.path.isdir(root_dirname)   # ...Murphy and her ironclad Law
+
+        # Append a path separator to this directory if needed.
+        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
+
+        # Test whether each path component split from this pathname is valid or
+        # not, ignoring non-existent and non-readable path components.
+        for pathname_part in pathname.split(os.path.sep):
+            try:
+                os.lstat(root_dirname + pathname_part)
+            except OSError as exc:
+                if hasattr(exc, 'winerror'):
+                    if exc.winerror == ERROR_INVALID_NAME:
+                        return False
+                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
+                    return False
+    except TypeError as exc:
+        return False
+    else:
+        return True
