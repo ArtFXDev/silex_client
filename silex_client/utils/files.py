579b0597c6d1f25f3750d2000b11513abe578e72
Joseph HENRY <josephenry@protonmail.com>
2021-12-16 17:16:24 +0100

    feat: slugify filename on conform

--- silex_client/utils/files.py
+++ silex_client/utils/files.py
@@ -11,6 +11,8 @@ import pathlib
 import re
 import sys
 import errno
+import unicodedata
+import re
 
 from types import ModuleType
 
@@ -109,3 +111,24 @@ def is_valid_path(pathname: str) -> bool:
         return False
     else:
         return True
+
+
+def slugify(value: str, allow_unicode=False) -> str:
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
