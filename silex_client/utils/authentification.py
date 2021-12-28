ac7a68817ec69bedf184748f4c53680121162ba2
Simon Lambin <slambin@artfx.fr>
2021-12-28 11:14:18 +0100

    Comment authentification

--- silex_client/utils/authentification.py
+++ silex_client/utils/authentification.py
@@ -14,6 +14,9 @@ from silex_client.utils.log import logger
 
 
 def is_authentificated() -> bool:
+    """
+    Test if the gazu client has already valid authentification tokens
+    """
     async def test_authentification():
         headers = gazu.client.default_client.headers
         async with aiohttp.ClientSession(headers=headers) as session:
@@ -30,6 +33,9 @@ def is_authentificated() -> bool:
 
 
 def authentificate_gazu() -> bool:
+    """
+    Get the zou authentificate_gazu token from the socket service
+    """
     gazu.set_host(os.getenv("SILEX_ZOU_HOST"))
 
     async def get_authentification_token():
