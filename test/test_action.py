80c6504834acabb7725a0eb68fb23480328c7b72
Simon Lambin <slambin@artfx.fr>
2021-12-31 16:31:19 +0100

    Add test for tester action

---
+++ test/test_action.py
@@ -0,0 +1,47 @@
+"""
+@author: TD gang
+
+Unit testing functions for the module core.context
+"""
+
+import os
+import pytest
+from concurrent import futures
+
+from silex_client.core.context import Context
+from silex_client.action.action_query import ActionQuery
+from silex_client.utils.enums import Status
+from silex_client.resolve.loader import Loader
+from .test_config import dummy_config
+
+
+@pytest.fixture
+def dummy_context() -> Context:
+    """
+    Return a context initialized in the test folder to work the configuration files
+    that has been created only for test purpose
+    """
+    context = Context.get()
+    context.update_metadata({"project": "TEST_PIPE"})
+    context.is_outdated = False
+    context.start_services()
+    return context
+
+
+def test_execute_action(dummy_context: Context):
+    """
+    Test the execution of all the commands in the 'testing' action
+    """
+    action = ActionQuery("tester", category="dev")
+    assert hasattr(action, "buffer")
+
+    action.set_parameter("parameter_tester:path:path_tester", "/foo/bar")
+    action.set_parameter("parameter_tester:path:path_tester_multiple", "/foo/bar")
+    action.set_parameter("parameter_tester:path:path_tester_extensions", "/foo/bar")
+    action.set_parameter("parameter_tester:path:path_tester_multiple_extensions", "/foo/bar")
+    future = action.execute()
+
+    # Let the execution of the action happen in the event loop thread
+    futures.wait([future])
+
+    assert action.status is Status.COMPLETED
