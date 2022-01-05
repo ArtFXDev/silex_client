2f70bbfe08c322480d13cc0808952c739ee54e0e
Simon Lambin <slambin@artfx.fr>
2022-01-05 20:41:02 +0100

    Handle already used port on websocket testing

--- test/test_websocket.py
+++ test/test_websocket.py
@@ -6,16 +6,21 @@ Unit testing functions for the module networK.websocket
 
 import asyncio
 import threading
+import aiohttp
 
 import pytest
 import socketio
-from aiohttp import web
+from aiohttp import web, test_utils
 
 from silex_client.core.context import Context
+from silex_client.network.websocket import WebsocketConnection
 
 from .mocks.websocket import DCCNamespace, ActionNamespace
 from .test_config import dummy_config
 from .test_action import dummy_context
+
+# Get an unused port
+TEST_PORT = test_utils.get_unused_port_socket("127.0.0.1").getsockname()[-1]
 
 
 @pytest.fixture
@@ -37,14 +42,13 @@ def dummy_server() -> threading.Thread:
         asyncio.set_event_loop(loop)
 
         loop.run_until_complete(runner.setup())
-        site = web.TCPSite(runner, host="127.0.0.1", port=5118)
+        site = web.TCPSite(runner, host="127.0.0.1", port=TEST_PORT)
         loop.run_until_complete(site.start())
         loop.run_forever()
 
     return threading.Thread(target=start_server, daemon=True, args=(runner,))
 
 
-@pytest.mark.skip(reason="The dummy_server implementation does not work on python 3.7")
 def test_connection_initialization(
     dummy_context: Context, dummy_server: threading.Thread
 ):
@@ -53,8 +57,11 @@ def test_connection_initialization(
     """
     dummy_server.start()
 
-    # Restart the services
+    # Restart the services on a new port
     dummy_context.stop_services()
+    dummy_context.ws_connection = WebsocketConnection(
+        f"ws://127.0.0.1:{TEST_PORT}", dummy_context
+    )
     dummy_context.start_services()
 
     assert dummy_context.ws_connection.is_running
