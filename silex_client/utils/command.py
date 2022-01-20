9968bf25ceee9afc8eb6852fa2a6d8c4c074bb93
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2022-01-20 11:32:22 +0100

    refactor command_builder

--- silex_client/utils/command.py
+++ silex_client/utils/command.py
@@ -8,7 +8,7 @@ class CommandBuilder:
 
     def __init__(self, executable: str):
         self.executable = executable
-        self.rez_packages = None
+        self.rez_cmd = None
         self.params: Dict[str, Optional[str]] = {}
 
     def param(self, key: str, value: Any = None):
@@ -17,7 +17,7 @@ class CommandBuilder:
         """
         self.params[key] = value
         return self
-
+    
     def disable(self, keys: List[str], false_value=0):
         """
         Set multiple keys with a false value
@@ -26,11 +26,11 @@ class CommandBuilder:
             self.param(key, false_value)
         return self
 
-    def add_rez_env(self, packages: List[str]):
+    def add_rez_env(self, packages: str):
         """
         Set the rez packages that will be resolved with this command
         """
-        self.rez_packages = packages
+        self.rez_cmd = ["rez", "env", packages, "--"]
 
     def as_argv(self) -> List[str]:
         """
@@ -42,10 +42,10 @@ class CommandBuilder:
         ]
         command: List[str] = [self.executable] + args
 
-        if self.rez_packages:
-            print(self.rez_packages)
+        if self.rez_cmd:
+            print(self.rez_cmd)
             print(command)
-            command = ["rez", "env"] + self.rez_packages + ["--"] + command
+            command = self.rez_cmd + command
 
         return command
 
