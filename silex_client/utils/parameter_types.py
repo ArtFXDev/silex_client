1755cb4d64b8cfaebf173051f010e57c4335e2c4
Simon Lambin <slambin@artfx.fr>
2022-01-21 12:01:35 +0100

    refactor all the parameters

--- silex_client/utils/parameter_types.py
+++ silex_client/utils/parameter_types.py
@@ -1,168 +1,25 @@
+# pylint: disable=C0103
+
 import pathlib
+from typing import List, Type
 
 from silex_client.utils.log import logger
+
+
+class CommandParameterMeta(type):
+    def __new__(cls, name: str, bases: tuple, dct: dict):
+        return super().__new__(cls, name, bases, dct)
+
+    def serialize(cls):
+        pass
+
+    def get_default(cls):
+        pass
 
 
 class AnyParameter(object):
     def __new__(cls, value):
         return value
-
-
-class CommandParameterMeta(type):
-    def __new__(cls, name: str, bases: tuple, dct: dict):
-        def serialize():
-            return {
-                "name": "parameter",
-            }
-
-        attributes = {
-            "serialize": serialize,
-        }
-        attributes.update(dct)
-        return super().__new__(cls, name, bases, attributes)
-
-    def get_default(self):
-        return None
-
-    def serialize(self):
-        return None
-
-
-class TaskParameterMeta(CommandParameterMeta):
-    def __init__(self):
-        pass
-
-    def __new__(cls):
-        def serialize():
-            return {
-                "name": "task",
-            }
-
-        def get_default():
-            return ""
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "TaskParameter", (str,), attributes)
-
-
-class IntArrayParameterMeta(CommandParameterMeta):
-    def __init__(self, size: int):
-        pass
-
-    def __new__(cls, size: int):
-        def __init__(self, value):
-            if not isinstance(value, list):
-                value = [value]
-
-            for index, item in enumerate(value):
-                value[index] = int(item)
-
-            self.extend(value)
-
-        def serialize():
-            return {
-                "name": "int_array",
-                "size": size,
-            }
-
-        def get_default():
-            return [0 for _ in range(size)]
-
-        attributes = {
-            "__init__": __init__,
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "IntArrayParameter", (list,), attributes)
-
-
-class RangeParameterMeta(CommandParameterMeta):
-    def __init__(self, start: int, end: int, increment: int = 1):
-        pass
-
-    def __new__(cls, start: int, end: int, increment: int = 1):
-        def serialize():
-            return {
-                "name": "range",
-                "start": start,
-                "end": end,
-                "increment": increment,
-            }
-
-        def get_default():
-            return start
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "RangeParameter", (int,), attributes)
-
-
-class SelectParameterMeta(CommandParameterMeta):
-    def __init__(self, *list_options, **options):
-        pass
-
-    def __new__(cls, *list_options, **options):
-        for unnamed_option in list_options:
-            options[unnamed_option] = unnamed_option
-
-        def serialize():
-            return {"name": "select", "options": options}
-
-        def get_default():
-            return list(options.values())[0] if options else None
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "SelectParameter", (str,), attributes)
-
-
-class RadioSelectParameterMeta(CommandParameterMeta):
-    def __init__(self, *list_options, **options):
-        pass
-
-    def __new__(cls, *list_options, **options):
-        for unnamed_option in list_options:
-            options[unnamed_option] = unnamed_option
-
-        def serialize():
-            return {"name": "radio_select", "options": options}
-
-        def get_default():
-            return list(options.values())[0] if options else None
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "RadioSelectParameter", (str,), attributes)
-
-
-class MultipleSelectParameterMeta(CommandParameterMeta):
-    def __init__(self, *list_options, **options):
-        pass
-
-    def __new__(cls, *list_options, **options):
-        for unnamed_option in list_options:
-            options[unnamed_option] = unnamed_option
-
-        def serialize():
-            return {"name": "multiple_select", "options": options}
-
-        def get_default():
-            return [list(options.values())[0]] if options else None
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
-        }
-        return super().__new__(cls, "SelectParameter", (list,), attributes)
 
 
 # TODO: Replace this parameter with ListParameterMeta
@@ -178,98 +35,195 @@ class ListParameter(list):
         self.extend(data)
 
 
-class PathParameterMeta(CommandParameterMeta):
-    def __init__(self, extensions=None, multiple=False):
-        pass
-
-    def __new__(cls, extensions=None, multiple=False):
-        if extensions is None:
-            extensions = ["*"]
-
-        def __init_list__(self, value):
-            if not isinstance(value, list):
-                value = [value]
-
-            for index, item in enumerate(value):
-                value[index] = pathlib.Path(item)
-
-            self.extend(value)
-
-        def serialize():
-            return {
-                "name": "Path",
-                "extensions": extensions,
-                "multiple": multiple,
-            }
-
-        def get_default():
-            return None
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
+def TaskParameterMeta():
+    def serialize():
+        return {
+            "name": "task",
         }
 
-        if multiple:
-            attributes["__init__"] = __init_list__
-            return super().__new__(cls, "PathParameter", (list,), attributes)
+    def get_default():
+        return ""
 
-        return super().__new__(
-            cls, "PathParameter", (type(pathlib.Path()),), attributes
-        )
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("TaskParameter", (str,), attributes)
 
 
-class ListParameterMeta(CommandParameterMeta):
-    def __init__(self, parameter_type):
-        pass
+def IntArrayParameterMeta(size: int):
+    def __init__(self, value):
+        if not isinstance(value, list):
+            value = [value]
 
-    def __new__(cls, parameter_type):
-        def __init__(self, value):
-            if not isinstance(value, list):
-                value = [value]
+        for index, item in enumerate(value):
+            value[index] = int(item)
 
-            for index, item in enumerate(value):
-                value[index] = parameter_type(item)
+        self.extend(value)
 
-            self.extend(value)
-
-        def serialize():
-            item_type = None
-
-            if isinstance(parameter_type, CommandParameterMeta):
-                return parameter_type.serialize()
-
-            elif isinstance(parameter_type, type):
-                item_type = {"name": parameter_type.__name__}
-
-            return {"name": "list", "itemtype": item_type}
-
-        def get_default():
-            return []
-
-        attributes = {
-            "__init__": __init__,
-            "serialize": serialize,
-            "get_default": get_default,
+    def serialize():
+        return {
+            "name": "int_array",
+            "size": size,
         }
 
-        return super().__new__(cls, "ListParameter", (list,), attributes)
+    def get_default():
+        return [0 for _ in range(size)]
+
+    attributes = {
+        "__init__": __init__,
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("IntArrayParameter", (list,), attributes)
 
 
-class TextParameterMeta(CommandParameterMeta):
-    def __init__(self, color=None):
-        pass
-
-    def __new__(cls, color=None):
-        def serialize():
-            return {"name": "text", "color": color}
-
-        def get_default():
-            return ""
-
-        attributes = {
-            "serialize": serialize,
-            "get_default": get_default,
+def RangeParameterMeta(start: int, end: int, increment: int = 1):
+    def serialize():
+        return {
+            "name": "range",
+            "start": start,
+            "end": end,
+            "increment": increment,
         }
 
-        return super().__new__(cls, "ListParameter", (str,), attributes)
+    def get_default():
+        return start
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("RangeParameter", (int,), attributes)
+
+
+def SelectParameterMeta(*list_options, **options):
+    for unnamed_option in list_options:
+        options[unnamed_option] = unnamed_option
+
+    def serialize():
+        return {"name": "select", "options": options}
+
+    def get_default():
+        return list(options.values())[0] if options else None
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("SelectParameter", (str,), attributes)
+
+
+def RadioSelectParameterMeta(*list_options, **options):
+    for unnamed_option in list_options:
+        options[unnamed_option] = unnamed_option
+
+    def serialize():
+        return {"name": "radio_select", "options": options}
+
+    def get_default():
+        return list(options.values())[0] if options else None
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("RadioSelectParameter", (str,), attributes)
+
+
+def MultipleSelectParameterMeta(*list_options, **options):
+    for unnamed_option in list_options:
+        options[unnamed_option] = unnamed_option
+
+    def serialize():
+        return {"name": "multiple_select", "options": options}
+
+    def get_default():
+        return [list(options.values())[0]] if options else None
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+    return CommandParameterMeta("SelectParameter", (list,), attributes)
+
+
+def PathParameterMeta(extensions: List[str] = None, multiple: bool = False):
+    if extensions is None:
+        extensions = ["*"]
+
+    def __init__(self, value):
+        if not isinstance(value, list):
+            value = [value]
+
+        for index, item in enumerate(value):
+            value[index] = pathlib.Path(item)
+
+        self.extend(value)
+
+    def serialize():
+        return {
+            "name": "Path",
+            "extensions": extensions,
+            "multiple": multiple,
+        }
+
+    def get_default():
+        return None
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+
+    if multiple:
+        attributes["__init__"] = __init__
+        return CommandParameterMeta("PathParameter", (list,), attributes)
+
+    return CommandParameterMeta("PathParameter", (type(pathlib.Path()),), attributes)
+
+
+def ListParameterMeta(parameter_type: Type):
+    def __init__(self, value):
+        if not isinstance(value, list):
+            value = [value]
+
+        for index, item in enumerate(value):
+            value[index] = parameter_type(item)
+
+        self.extend(value)
+
+    def serialize():
+        item_type = None
+
+        item_type = {"name": parameter_type.__name__}
+        if isinstance(parameter_type, CommandParameterMeta):
+            item_type = parameter_type.serialize()
+
+        return {"name": "list", "itemtype": item_type}
+
+    def get_default():
+        return []
+
+    attributes = {
+        "__init__": __init__,
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+
+    return CommandParameterMeta("ListParameter", (list,), attributes)
+
+
+def TextParameterMeta(color=None):
+    def serialize():
+        return {"name": "text", "color": color}
+
+    def get_default():
+        return ""
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+    }
+
+    return CommandParameterMeta("ListParameter", (str,), attributes)
