96ca6a395d4eaab251b6c1dc58de19f1b2134542
Simon Lambin <slambin@artfx.fr>
2022-01-28 10:06:14 +0100

    Add unittests with conform file sequences

--- test/test_conform.py
+++ test/test_conform.py
@@ -18,11 +18,13 @@ from silex_client.utils.enums import Status
 conform_types = [
     i["name"] for i in Config.get().get_actions("conform") if i["name"] != "default"
 ]
+file_quantities = [1, 19]
 
 
 @pytest.mark.parametrize("conform_type", conform_types)
+@pytest.mark.parametrize("file_quantity", file_quantities)
 def test_execute_conform_action(
-    mocker: MockFixture, tmp_path: pathlib.Path, conform_type: str
+    mocker: MockFixture, tmp_path: pathlib.Path, conform_type: str, file_quantity: int
 ):
     """
     Test the execution of all the commands in the 'conform' action
@@ -34,10 +36,17 @@ def test_execute_conform_action(
     mock.__getitem__.side_effect = lambda x: getattr(mock, x)
 
     output_path = tmp_path / "conform"
-    input_path = tmp_path / f"unittest_conform.{conform_type}"
-    final_path = (
-        tmp_path / "unittest_conform" / f"conform_unittest_conform.{conform_type}"
-    )
+    input_path = [
+        tmp_path
+        / f"unittest_conform{'.' + str(i).zfill(4) if file_quantity > 1 else ''}.{conform_type}"
+        for i in range(file_quantity)
+    ]
+    final_path = [
+        tmp_path
+        / "unittest_conform"
+        / f"conform_unittest_conform{'.' + str(i).zfill(4) if file_quantity > 1 else ''}.{conform_type}"
+        for i in range(file_quantity)
+    ]
 
     event_loop = Context.get().event_loop.loop
     mock_future = event_loop.create_future()
@@ -60,8 +69,9 @@ def test_execute_conform_action(
     )
 
     # Set the parameter that are required for the execution
-    input_path.parent.mkdir(exist_ok=True)
-    input_path.touch(exist_ok=True)
+    for path in input_path:
+        path.parent.mkdir(exist_ok=True)
+        path.touch(exist_ok=True)
     action.set_parameter("setup:get_conform_output:file_paths", input_path)
     future = action.execute(batch=True)
 
@@ -69,4 +79,5 @@ def test_execute_conform_action(
     future.result()
 
     assert action.status is Status.COMPLETED
-    assert final_path.exists()
+    for path in final_path:
+        assert path.exists()
