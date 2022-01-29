67de41dc6ada6dafe7df0d9f689bc029b70360ed
Simon Lambin <slambin@artfx.fr>
2022-01-29 15:09:32 +0100

    Edit the parameter types and create the UnionParameterMeta

--- silex_client/utils/parameter_types.py
+++ silex_client/utils/parameter_types.py
@@ -1,44 +1,63 @@
+"""
+Defintions of all the available parameter input types.
+Parameter's input types can also be a regular class definition but these
+constructors gives more control
+"""
 # pylint: disable=C0103
+from __future__ import annotations
 
 import pathlib
-from typing import List, Type
+import contextlib
+from typing import List, Type, Dict, Any
 
-from silex_client.utils.log import logger
 
-
-class CommandParameterMeta(type):
+class ParameterInputTypeMeta(type):
+    """
+    Every parameter have an input type.
+    The input type can either be a regular class definition or an instance of
+    this metaclass. The advantage of this metaclass is that it returns additional
+    information about the type
+    """
     def __new__(cls, name: str, bases: tuple, dct: dict):
         return super().__new__(cls, name, bases, dct)
 
-    def serialize(cls):
-        pass
+    def serialize(cls) -> Dict[str, Any]:
+        """
+        When serializing the parameter, this mehod can be overriden to send additional
+        information about the parameter type
+        """
+        return {"name": cls.__name__}
 
-    def get_default(cls):
-        pass
+    @staticmethod
+    def get_default() -> Any:
+        """
+        When no value is given on a parameter, the default value will be None.
+        You can override this method to define the default value that will be set for the
+        parameter
+        """
+        return None
 
-    def rebuild(cls, *args, **kwargs):
-        pass
+    def rebuild(cls) -> ParameterInputTypeMeta:
+        """
+        Parameter types definitions are immutable
+        To change their attributes, you can use this method to rebuild it
+        """
+        return cls("InvalidParameter", (type(None), ), {})
 
 
 class AnyParameter(object):
+    """
+    Parameter type that allows any value to be passed through
+    """
     def __new__(cls, value):
         return value
 
 
-# TODO: Replace this parameter with ListParameterMeta
-class ListParameter(list):
-    def __init__(self, value):
-        logger.warning(
-            "Deprecation warning: The parameter type ListParameter is deprecated in favor if ListParameterMeta()"
-        )
-        data = value
-
-        if not isinstance(value, list):
-            data = [value]
-        self.extend(data)
-
-
 def TaskParameterMeta():
+    """
+    Builds a parameter type that returns a task id as a string
+    In the UI, this parameter is a task selector on the current project
+    """
     def serialize():
         return {
             "name": "task",
@@ -52,16 +71,23 @@ def TaskParameterMeta():
         "get_default": get_default,
         "rebuild": TaskParameterMeta,
     }
-    return CommandParameterMeta("TaskParameter", (str,), attributes)
+    return ParameterInputTypeMeta("TaskParameter", (str,), attributes)
 
 
 def IntArrayParameterMeta(size: int):
+    """
+    Builds a parameter type that returns a list of integers with the given lenght
+    In the UI, this parameter is multiple integer fields
+    """
     def __init__(self, value):
         if not isinstance(value, list):
             value = [value]
 
         for index, item in enumerate(value):
             value[index] = int(item)
+
+        if not len(value) == size:
+            raise TypeError(f"The value {value} does not have the right lenght ({size})")
 
         self.extend(value)
 
@@ -80,10 +106,21 @@ def IntArrayParameterMeta(size: int):
         "get_default": get_default,
         "rebuild": IntArrayParameterMeta,
     }
-    return CommandParameterMeta("IntArrayParameter", (list,), attributes)
+    return ParameterInputTypeMeta("IntArrayParameter", (list,), attributes)
 
 
 def RangeParameterMeta(start: int, end: int, increment: int = 1):
+    """
+    Builds a parameter type that returns an integer clipped in the given range
+    In the UI, this parameter is a slider with the given limits
+    """
+    def __new__(cls, value):
+        value = int(value)
+
+        if value < start or value > end:
+            raise TypeError("The value {value} does not fit the expexted range ({start}:{end})")
+        return int.__new__(cls, value)
+
     def serialize():
         return {
             "name": "range",
@@ -96,34 +133,30 @@ def RangeParameterMeta(start: int, end: int, increment: int = 1):
         return start
 
     attributes = {
+        "__new__": __new__,
         "serialize": serialize,
         "get_default": get_default,
         "rebuild": RangeParameterMeta,
     }
-    return CommandParameterMeta("RangeParameter", (int,), attributes)
-
-
-def SelectParameterMeta(*list_options, **options):
-    for unnamed_option in list_options:
-        options[unnamed_option] = unnamed_option
-
-    def serialize():
-        return {"name": "select", "options": options}
-
-    def get_default():
-        return list(options.values())[0] if options else None
-
-    attributes = {
-        "serialize": serialize,
-        "get_default": get_default,
-        "rebuild": SelectParameterMeta,
-    }
-    return CommandParameterMeta("SelectParameter", (str,), attributes)
+    return ParameterInputTypeMeta("RangeParameter", (int,), attributes)
 
 
 def RadioSelectParameterMeta(*list_options, **options):
+    """
+    Builds a parameter type that returns a string in the given options
+    In the UI, this parameter is a radio select to pick an option
+    The options can either be a list or a dict. In the case of a dict,
+    the keys will be the displayed string and the values the returned ones
+    """
     for unnamed_option in list_options:
         options[unnamed_option] = unnamed_option
+
+    def __new__(cls, value):
+        value = str(value)
+        if value not in options.values():
+            raise TypeError(f"The value {value} is not among the possible options ({options})")
+        return str.__new__(cls, value)
+
 
     def serialize():
         return {"name": "radio_select", "options": options}
@@ -132,16 +165,63 @@ def RadioSelectParameterMeta(*list_options, **options):
         return list(options.values())[0] if options else None
 
     attributes = {
+        "__new__": __new__,
         "serialize": serialize,
         "get_default": get_default,
         "rebuild": RadioSelectParameterMeta,
     }
-    return CommandParameterMeta("RadioSelectParameter", (str,), attributes)
+    return ParameterInputTypeMeta("RadioSelectParameter", (str,), attributes)
+
+
+def SelectParameterMeta(*list_options, **options):
+    """
+    Builds a parameter type that returns a string in the given options
+    In the UI, this parameter is a dropdown menu.
+    The options can either be a list or a dict. In the case of a dict,
+    the keys will be the displayed string and the values the returned ones
+    """
+    for unnamed_option in list_options:
+        options[unnamed_option] = unnamed_option
+
+    def __new__(cls, value):
+        value = str(value)
+        if value not in options.values():
+            raise TypeError(f"The value {value} is not among the possible options ({options})")
+        return str.__new__(cls, value)
+
+    def serialize():
+        return {"name": "select", "options": options}
+
+    def get_default():
+        return list(options.values())[0] if options else None
+
+    attributes = {
+        "__new__": __new__,
+        "serialize": serialize,
+        "get_default": get_default,
+        "rebuild": SelectParameterMeta,
+    }
+    return ParameterInputTypeMeta("SelectParameter", (str,), attributes)
 
 
 def MultipleSelectParameterMeta(*list_options, **options):
+    """
+    Build same parameter as SelectParameterMeta but return a list of strings
+    In the UI, the user can select multiple options
+    """
     for unnamed_option in list_options:
         options[unnamed_option] = unnamed_option
+
+    def __init__(self, values):
+        if not isinstance(values, list):
+            values = [values]
+
+        for value in values:
+            value = str(value)
+            if value not in options.values():
+                raise TypeError(f"The value {value} is not among the possible options ({options})")
+
+        self.extend([str(value) for value in values])
 
     def serialize():
         return {"name": "multiple_select", "options": options}
@@ -150,14 +230,19 @@ def MultipleSelectParameterMeta(*list_options, **options):
         return [list(options.values())[0]] if options else None
 
     attributes = {
+        "__ini__": __init__,
         "serialize": serialize,
         "get_default": get_default,
         "rebuild": MultipleSelectParameterMeta,
     }
-    return CommandParameterMeta("SelectParameter", (list,), attributes)
+    return ParameterInputTypeMeta("SelectParameter", (list,), attributes)
 
 
 def PathParameterMeta(extensions: List[str] = None, multiple: bool = False):
+    """
+    Builds a parameter type that returns a pathlib path
+    In the UI, this parameter is a file selector
+    """
     if extensions is None:
         extensions = ["*"]
 
@@ -188,12 +273,64 @@ def PathParameterMeta(extensions: List[str] = None, multiple: bool = False):
 
     if multiple:
         attributes["__init__"] = __init__
-        return CommandParameterMeta("PathParameter", (list,), attributes)
+        return ParameterInputTypeMeta("PathParameter", (list,), attributes)
 
-    return CommandParameterMeta("PathParameter", (type(pathlib.Path()),), attributes)
+    return ParameterInputTypeMeta("PathParameter", (pathlib.Path,), attributes)
+
+
+def TextParameterMeta(color=None):
+    """
+    Builds a parameter type that return a string
+    In the UI, this parameter display the string with the given color
+    """
+    def serialize():
+        return {"name": "text", "color": color}
+
+    def get_default():
+        return ""
+
+    attributes = {
+        "serialize": serialize,
+        "get_default": get_default,
+        "rebuild": TextParameterMeta,
+    }
+
+    return ParameterInputTypeMeta("ListParameter", (str,), attributes)
+
+
+def StringParameterMeta(multiline: bool = False, max_lenght: int = 1000, readonly: bool = False):
+    """
+    Builds a parameter type that return a string that will be trimed if too long
+    In the UI, this parameter display a string field
+    """
+    def __new__(cls, value):
+        value = str(value)
+        # Trim the name with the max lenght
+        value = value[:max_lenght]
+        return str.__new__(cls, value)
+
+    def serialize():
+        return {"name": "str", "multiline": multiline, "maxLenght": max_lenght, "readonly": readonly}
+
+    def get_default():
+        return ""
+
+    attributes = {
+        "__new__": __new__,
+        "serialize": serialize,
+        "get_default": get_default,
+        "rebuild": StringParameterMeta,
+    }
+
+    return ParameterInputTypeMeta("StringParameter", (str,), attributes)
 
 
 def ListParameterMeta(parameter_type: Type):
+    """
+    This parameter takes an other parameter as option
+    Builds a parameter type that return a list of the given type
+    In the UI, this parameter show the widget of the given type in a list
+    """
     def __init__(self, value):
         if not isinstance(value, list):
             value = [value]
@@ -207,7 +344,7 @@ def ListParameterMeta(parameter_type: Type):
         item_type = None
 
         item_type = {"name": parameter_type.__name__}
-        if isinstance(parameter_type, CommandParameterMeta):
+        if isinstance(parameter_type, ParameterInputTypeMeta):
             item_type = parameter_type.serialize()
 
         return {"name": "list", "itemType": item_type}
@@ -222,49 +359,15 @@ def ListParameterMeta(parameter_type: Type):
         "rebuild": ListParameterMeta,
     }
 
-    return CommandParameterMeta("ListParameter", (list,), attributes)
-
-
-def TextParameterMeta(color=None):
-    def serialize():
-        return {"name": "text", "color": color}
-
-    def get_default():
-        return ""
-
-    attributes = {
-        "serialize": serialize,
-        "get_default": get_default,
-        "rebuild": TextParameterMeta,
-    }
-
-    return CommandParameterMeta("ListParameter", (str,), attributes)
-
-
-def StringParameterMeta(multiline: bool = False, max_lenght: int = 1000):
-    def __new__(cls, value):
-        value = str(value)
-        # Trim the name with the max lenght
-        value = value[:max_lenght]
-        return str.__new__(cls, value)
-
-    def serialize():
-        return {"name": "str", "multiline": multiline, "maxLenght": max_lenght}
-
-    def get_default():
-        return ""
-
-    attributes = {
-        "__new__": __new__,
-        "serialize": serialize,
-        "get_default": get_default,
-        "rebuild": StringParameterMeta,
-    }
-
-    return CommandParameterMeta("StringParameter", (str,), attributes)
+    return ParameterInputTypeMeta("ListParameter", (list,), attributes)
 
 
 def DictParameterMeta(key_type: Type, value_type: Type):
+    """
+    This parameter takes other parameters as options
+    Builds a parameter type that return a dict of the given types
+    In the UI, this parameter show the widget of the given types in a key/value manner
+    """
     def __init__(self, input_value):
         # Cast the input into a dict
         if not isinstance(input_value, dict):
@@ -283,11 +386,11 @@ def DictParameterMeta(key_type: Type, value_type: Type):
 
     def serialize():
         key_serialized = {"name": key_type.__name__}
-        if isinstance(key_type, CommandParameterMeta):
+        if isinstance(key_type, ParameterInputTypeMeta):
             key_serialized = key_type.serialize()
 
         value_serialized = {"name": value_type.__name__}
-        if isinstance(value_type, CommandParameterMeta):
+        if isinstance(value_type, ParameterInputTypeMeta):
             value_serialized = value_type.serialize()
 
         return {"name": "dict", "key": key_serialized, "value": value_serialized}
@@ -302,4 +405,54 @@ def DictParameterMeta(key_type: Type, value_type: Type):
         "rebuild": DictParameterMeta,
     }
 
-    return CommandParameterMeta("ListParameter", (dict,), attributes)
+    return ParameterInputTypeMeta("ListParameter", (dict,), attributes)
+
+
+def UnionParameterMeta(parameter_types: List[Type]):
+    """
+    This parameter takes other parameters as options
+    Builds a parameter type that return a dict of the given types
+    In the UI, this parameter show the widget of the given types in a key/value manner
+    """
+    # Remove the double parameters types
+    parameter_types = list(set(parameter_types))
+
+    @staticmethod
+    def __new__(value):
+        # Test if the value match one of the types
+        for parameter_type in parameter_types:
+            if isinstance(value, parameter_type):
+                return value
+
+        # Try to cast the value into one of the types
+        for parameter_type in parameter_types:
+            with contextlib.suppress(Exception):
+                return parameter_type(value)
+
+        raise TypeError(f"None of the types matches the value {value}")
+
+    def serialize():
+        serialized_parameter_types = []
+        for parameter_type in parameter_types:
+            serialized_parameter_type = {"name": parameter_type.__name__}
+            if isinstance(parameter_type, ParameterInputTypeMeta):
+                serialized_parameter_type = parameter_type.serialize()
+
+            serialized_parameter_types.append(serialized_parameter_type)
+            serialized_parameter_types = list(set(serialized_parameter_types))
+
+        return {"name": "union", "types": serialized_parameter_types}
+
+    def get_default():
+        if parameter_types and isinstance(parameter_types[0], ParameterInputTypeMeta):
+            return parameter_types[0].get_default()
+        return None
+
+    attributes = {
+        "__new__": __new__,
+        "serialize": serialize,
+        "get_default": get_default,
+        "rebuild": UnionParameterMeta,
+    }
+
+    return ParameterInputTypeMeta("ListParameter", (object,), attributes)
