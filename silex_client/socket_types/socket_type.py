bd322af4f457cdb7e7dc9d54e7850fbf3d83c3df
Simon Lambin <slambin@artfx.fr>
2022-04-08 19:07:04 +0200

    Type the SocketType class

--- silex_client/socket_types/socket_type.py
+++ silex_client/socket_types/socket_type.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
 
+from typing import TypeVar, Generic
 
-class SocketType(ABC):
+T = TypeVar("T")
+
+
+class SocketType(ABC, Generic[T]):
     @abstractmethod
     def get_default(self):
         pass
 
+    @abstractmethod
+    def cast(self, value) -> T:
+        pass
+
     def rebuild(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
--- silex_client/socket_types/void_socket.py
+++ silex_client/socket_types/void_socket.py
@@ -1,6 +1,9 @@
 from silex_client.socket_types.socket_type import SocketType
 
 
-class VoidSocket(SocketType):
+class VoidSocket(SocketType[None]):
     def get_default(self):
+        return None
+
+    def cast(self, value):
         return None
