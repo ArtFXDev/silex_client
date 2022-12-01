5c0e7925a26528299955cdfca7e1856f7301acee
Elise <72398192+EFV0804@users.noreply.github.com>
2022-12-01 17:56:53 +0100

    added deadline submit, submits dummy job, no param

--- silex_client/commands/farm/submit_to_deadline.py
+++ silex_client/commands/farm/submit_to_deadline.py
@@ -18,8 +18,44 @@ from silex_client.utils.parameter_types import (
 # Forward references
 if typing.TYPE_CHECKING:
     from silex_client.action.action_query import ActionQuery
-
+  
+from Deadline.DeadlineConnect import DeadlineCon
 
 
 class SubmitToDeadlineCommand(CommandBase):
-  pass
+    """
+    Send job to Deadline
+    """
+
+    parameters = {
+        "tasks": {
+            "label": "Tasks list",
+            "type": ListParameterMeta(farm.Task),
+            "hide": True,
+        }}
+
+    @CommandBase.conform_command()
+    async def __call__(
+        self,
+        parameters: Dict[str, Any],
+        action_query: ActionQuery,
+        logger: logging.Logger,
+    ):
+        deadline = DeadlineCon('localhost', 8081)
+
+        JobInfo = {
+            "Name": "CommandLine submit test",
+            "UserName": "elise.vidal",
+            "Frames": "0-1",
+            "Plugin": "CommandLine"
+
+        }
+
+        PluginInfo = {
+            "Executable": "C:\\rez\\__install__\\Scripts\\rez\\rez-env.exe",
+            "Arguments": '''testpipe python -- python -c "print('hello')"'''
+        }
+
+        logger.debug("deadline submit script called")
+
+        new_job = deadline.Jobs.SubmitJob(JobInfo, PluginInfo)
