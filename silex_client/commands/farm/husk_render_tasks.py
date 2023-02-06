8fcd42326b7be4d044dcde2cfd7e204f20ca1f95
angele.sionneau <asionneau@artfx.fr>
2023-02-06 18:29:55 +0100

    modify husk_render_task, to be approved (WIP)

--- silex_client/commands/farm/husk_render_tasks.py
+++ silex_client/commands/farm/husk_render_tasks.py
@@ -4,6 +4,7 @@ import logging
 import pathlib
 import typing
 from typing import Any, Dict, List, cast
+from silex_client.utils.log import flog
 
 from fileseq import FrameSet
 from silex_client.action.command_base import CommandBase
@@ -32,18 +33,22 @@ class HuskRenderTasksCommand(CommandBase):
         },
         "frame_range": {
             "label": "Frame range (start, end, step)",
-            "type": IntArrayParameterMeta(3),
-            "value": [1, 50, 1],
+            #"type": IntArrayParameterMeta(3),
+            #"value": [1, 50, 1],
+            "type": FrameSet,
+            "value": "1-50x1",
         },
-        "task_size": {
-            "label": "Task size",
-            "tooltip": "Number of frames per computer",
+        #"output_directory": {"type": pathlib.Path, "hide": True, "value": ""},
+        #"output_filename": {"type": pathlib.Path, "hide": True, "value": ""},
+        #"output_extension": {"type": str, "hide": True, "value": "exr"},
+        "output_directory": {"type": pathlib.Path, "hide": False, "value": ""},
+        "output_filename": {"type": pathlib.Path, "hide": False, "value": ""},
+        "output_extension": {"type": str, "hide": False, "value": "exr"},
+        "task_size" : {
+            "label": "Task Size",
             "type": int,
-            "value": 10,
-        },
-        "output_directory": {"type": pathlib.Path, "hide": True, "value": ""},
-        "output_filename": {"type": pathlib.Path, "hide": True, "value": ""},
-        "output_extension": {"type": str, "hide": True, "value": "exr"},
+            "value" : 10
+        }
     }
 
     @CommandBase.conform_command()
@@ -54,9 +59,8 @@ class HuskRenderTasksCommand(CommandBase):
         logger: logging.Logger,
     ):
         scene: pathlib.Path = parameters["scene_file"]
-        frame_range: List[int] = parameters["frame_range"]
-        task_size: int = parameters["task_size"]
-        full_path = f"{(parameters['output_directory'] / parameters['output_filename']).as_posix()}.$F4.{parameters['output_extension']}"
+
+        full_path = f"\{(parameters['output_directory'] / parameters['output_filename']).as_posix()}.$F4.{parameters['output_extension']}"
 
         husk_cmd = command_builder.CommandBuilder(
             "husk",
@@ -70,38 +74,21 @@ class HuskRenderTasksCommand(CommandBase):
         husk_cmd.param("exrmode", 1)
         husk_cmd.param("verbose", "3a")
 
-        tasks: List[farm.Task] = []
+        #set cmd frames
+        frame_range: List[int] = parameters["frame_range"]
+        frame_range = str(frame_range)
+        increment = frame_range[0].split("x")[-1]
+        task_size: int = parameters["task_size"]
+        task_size = str(task_size)
 
-        # Split frames by task size
-        frame_chunks = split_frameset(
-            FrameSet.from_range(*frame_range),
-            task_size,
-        )
 
-        # Creating tasks for each frame chunk
-        for chunk in frame_chunks:
-            chunk_cmd = husk_cmd.deepcopy()
 
-            start = cast(int, chunk[0])
-            end = cast(int, chunk[-1])
-            step = frame_range[2]
+        husk_cmd.param("frame", "<STARTFRAME>")
+        flog.info("add frame param")
+        husk_cmd.param("frame-inc", increment)
+        flog.info("add frame increment")
+        husk_cmd.param("frame-count", task_size)
+        flog.info("add frame count")
 
-            chunk_cmd.param("frame", start)
-            chunk_cmd.param("frame-count", end - start + 1)
-            chunk_cmd.param("frame-inc", step)
-
-            task = farm.Task(title=str(chunk))
-
-            command = farm.wrap_command(
-                [
-                    farm.get_mount_command(
-                        action_query.context_metadata.get("project_nas")
-                    ),
-                    farm.get_clear_frames_command(pathlib.Path(full_path), chunk),
-                ],
-                cmd=farm.Command(chunk_cmd.as_argv()),
-            )
-            task.addCommand(command)
-            tasks.append(task)
-
-        return {"tasks": tasks, "file_name": scene.stem}
+        flog.info(husk_cmd)
+        return {"command": husk_cmd, "file_name": scene.stem, "frame_range": parameters["frame_range"].frameRange()}
